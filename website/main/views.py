755752f35838f1cc0b3c4a57e6d90b5c12d36dc5
Dzianis Bohush <110992506+densparrow@users.noreply.github.com>
2022-08-27 11:06:19 +0300

    создал файл forms.py, в нем описал формы и добавил их в create.html

--- website/main/views.py
+++ website/main/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
+
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-created_on')
     created_on = Task.objects.all()
     return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': tasks, 'created_on': created_on})
 
@@ -16,4 +18,17 @@ def my_page(request):
 
 
 def create(request):
-    return render(request, 'main/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
